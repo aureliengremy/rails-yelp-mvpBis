d948df11d45c3ce270e5fa43cccc862f28728395
Aurelien Gremy <gremy.aurelien@gmail.com>
2022-05-11 15:43:56 -0400

    rails-yelp-mvp : working with poor style

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+
+Restaurant.create({name:'Aux vivres', address:'Street Vivres', phone_number:'438-406-1029', category:'italian'})
+Restaurant.create({name:'China', address:'St.China', phone_number:'438-204-3849', category:'chinese'})
+Restaurant.create({name:'Italia', address:'St.Italia', phone_number:'439-094-2932', category:'italian'})
+Restaurant.create({name:'Sushi', address:'St.Jap', phone_number:'948-948-3882', category:'japanese'})
+Restaurant.create({name:'Le Francais', address:'St.French', phone_number:'439-039-3838', category:'french'})
